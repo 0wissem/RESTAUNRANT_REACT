4652a037c26e6d741616c5ac6a953b59933d8354
Dhouha <dhouha.lahbib@gmail.com>
2022-08-29 21:07:15 +0100

    buttonShopNavBarOrderClient

---
+++ src/screens/orders/orderClient.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const OrderClient = () => {
+  return (
+    <div>
+      <h2>
+        <i className="fas fa-clipboard me-3"></i>Order of Client XX
+      </h2>
+
+      <div className="line "></div>
+
+      <div className="row justify-content-center"></div>
+    </div>
+  );
+};
+export default OrderClient;
